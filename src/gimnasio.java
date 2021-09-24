7ec894121b02edbc3ecc54bcb3983bb181f65a87
OscarFlorez <66635225+Santiago-Florez@users.noreply.github.com>
2021-09-24 18:19:14 -0500

    Creando proyecto

---
+++ src/gimnasio.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class gimnasio {
+
+    private static Scanner leer = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int cubiculo = leer.nextInt();
+        int casillero = leer.nextInt();
+        String ubicaciones[][]= new String[cubiculo][casillero];
+    }
+}
